7ff53cc3dc868d280553c31a342f073700b5efe8
Serg Osipchuk <serg.osipchuk@rapidapi.com>
2016-11-24 14:08:00 +0200

    package

--- test/Functional/AmazonMerchantTest.php
+++ test/Functional/AmazonMerchantTest.php
@@ -2,6 +2,8 @@
 
 namespace Test\Functional;
 
+require_once(__DIR__ . '/../../src/Models/checkRequest.php');
+
 class AmazonMerchantTest extends BaseTestCase {
     
     public function testGetEligibleShippingServices() {
